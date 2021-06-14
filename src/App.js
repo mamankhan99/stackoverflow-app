a519a2948c0dd11ef480fb3b12fdd0a48d5cf9b0
mamankhan99 <mamankhan99@gmail.com>
2021-06-14 19:07:03 +0500

    Theme warning fixed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import SignIn from './components/authentication/SignIn';
 import PublicRoute from './components/routes/PublicRoute';
-import {ThemeProvider} from '@material-ui/core';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core';
+import SignUp from './components/registration/SignUp';
+
+const theme = createMuiTheme({
+  mixins:{
+    toolbar:{
+      height: 54,
+    }
+  },
+})
 
 function App() {
   return (
     <Router>
-      <ThemeProvider>
+      <ThemeProvider theme={theme}>
         <div className="App">
             <Switch>              
               <PublicRoute component={SignIn} exact path='/login'/>
+              <PublicRoute component={SignUp} exact path='/signup'/>
             </Switch>
         </div>
       </ThemeProvider>
